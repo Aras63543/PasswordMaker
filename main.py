2e6fe81c86bece76ca23eb22ed3d08cbe6149e96
Aras63543 <arassemegir@protonmail.com>
2025-04-15 21:26:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import random
+import string
 
+length = 15
+
+characters = string.punctuation + string.hexdigits 
+
+password = ""
+for i in range(length):
+    random_character = random.choice(characters)
+    password = password + random_character
+
+print("Your password is: " + password)
